8ed479fd3b01ec207f04eb6776c4676c24c853fc
Carlos Vega Meyer <carlosvega09@gmail.com>
2017-06-08 14:49:45 -0300

    Adds /gm/pipe endpoint

--- gmUtils.js
+++ gmUtils.js
@@ -2,7 +2,56 @@ var fs = require('fs');
 var gm = require('gm');
 var request = require('request');
 var tmp = require('tmp');
+var url = require('url');
 var Promise = require('bluebird');
+
+var compact = xs => xs.filter(x => !!x);
+
+var isUrlRemote = urlString => {
+  var parsedUrl = url.parse(urlString);
+  return parsedUrl && !!parsedUrl.protocol.match(/https?:/);
+}
+
+var commandifyOptions = options => 
+  Object.entries(options || {}).reduce((acc, [k, v]) => [...acc, `-${k}`, v], []);
+
+var getTmpFilePath = () =>
+  new Promise((resolve, reject) => {
+    tmp.file((tmpError, path, fd, cleanup) => {
+      if (tmpError) return reject(tmpError);
+      resolve(path);
+    });
+  });
+
+var downloadImage = url => 
+  getTmpFilePath().then(
+    path => new Promise((resolve, reject) => {
+      var response = request(url);
+      response.pipe(fs.createWriteStream(path));
+      response.on('end', () => resolve(path));
+      response.on('error', reject);
+    })
+  );
+
+var dumpStreamToTmpFile = stream => 
+  getTmpFilePath().then(
+    path => new Promise((resolve, reject) => {
+      if (!stream) return reject(`stream is ${stream}`);
+      stream.pipe(fs.createWriteStream(path));
+      stream.on('end', () => resolve(path));
+      stream.on('error', reject);
+    })
+  );
+  
+var imageWithOptions = (context = {}) => ({ options, url: imageUrl = '' }) => {
+  var getImagePath = Promise.resolve(imageUrl);
+  if (imageUrl === "$_") {
+    getImagePath = dumpStreamToTmpFile(context.inputStream);
+  } else if (isUrlRemote(imageUrl)) {
+    getImagePath = downloadImage(imageUrl);
+  }
+  return getImagePath.then(path => compact([ path, ...commandifyOptions(options) ]));
+}
 
 /*
   {
@@ -19,48 +68,34 @@ var Promise = require('bluebird');
     }
   }
 */
+var composite = (context = {}) => (params = {}) => new Promise((resolve, reject) => {
+  Promise.all([params.imageA, params.imageB].map(imageWithOptions(context))).then(
+    ([ imageA, imageB ]) => {
+      var result = gm().command('composite')
+                       .in(...commandifyOptions(params.options))
+                       .in(...imageA)
+                       .in(...imageB);
 
-var compact = xs => xs.filter(x => !!x);
-
-var downloadImage = url => new Promise((resolve, reject) => {
-  tmp.file((tmpError, path, fd, cleanup) => {
-    if (tmpError) return reject(tmpError);
-
-    var response = request(url);
-    response.pipe(fs.createWriteStream(path));
-    response.on('end', () => resolve(path));
-    response.on('error', reject);
-  });
-});
-
-var composite = (params = {}) => new Promise((resolve, reject) => {
-  console.log(params)
-
-  var serializeOptions = options => 
-    Object.entries(options || {}).reduce((acc, [k, v]) => [...acc, `-${k}`, v], []);
-    
-  var imageWithOptions = ({ options, url }) => 
-    downloadImage(url).then(path => compact([ path, ...serializeOptions(options) ]));
-
-  Promise.all([params.imageA, params.imageB].map(imageWithOptions))
-    .then(
-      ([ imageA, imageB ]) => {
-        var result = gm().command('composite')
-                         .in(...serializeOptions(params.options))
-                         .in(...imageA)
-                         .in(...imageB);
-
-        result.stream((err, outputStream, _) => {
-          if (err) return reject(err);
-          resolve(outputStream);
-        });
-      },
-      error => {
-        console.error(error.message)
-        reject(error)
+      result.stream((err, outputStream, _) => {
+        if (err) return reject(err);
+        resolve(outputStream);
       });
+    },
+    error => {
+      console.error(error.message)
+      reject(error)
+    }
+  );
 });
+
+var convert = (context = {}) => (params = {}) =>
+  imageWithOptions(params.image).then(
+    image => {
+      // TODO
+    }
+  );
 
 module.exports = {
-  composite: composite,
+  composite,
+  convert
 };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
-
 var gmUtils = require('./gmUtils.js')
+var Promise = require('bluebird');
 
 app.use(bodyParser.json());
 
@@ -21,6 +21,30 @@ app.post('/gm/composite', (req, res) =>
       res.send(error.message);
     }
   ));
+
+/*
+  curl localhost:85/gm/pipe \
+    -d "$(cat example.pipe.json | sed s/\"/\\\"/g)" \
+    -H 'Content-Type: application/json'
+*/
+app.post('/gm/pipe', (req, res) => {
+  var pipe = req.body.pipe || [];
+
+  Promise.reduce(
+    pipe,
+    (inputStream, action) => gmUtils[action.utility]({ inputStream })(action),
+    Promise.resolve(null)
+  ).then(
+    outputStream => {
+      res.setHeader('Content-Type', 'image');
+      outputStream.pipe(res);
+    },
+    error => {
+      res.status(500);
+      res.send(error.message);
+    }
+  );
+});
 
 app.listen(80);
 console.log("Listening on port 80");
