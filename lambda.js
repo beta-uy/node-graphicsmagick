dd1b4e69cbc8f56be157ea1a9b820c6fb7b73a42
Carlos Vega Meyer <carlosvega09@gmail.com>
2017-07-08 19:18:24 -0300

    Can't get any simpler can it

--- lambda.js
+++ lambda.js
@@ -1,26 +1,42 @@
-var gmUtils = require('./gmUtils');
+var request = require('request');
+var gm = require('gm').subClass({ imageMagick: true });
 
 exports.handler = (event, context, callback) => {
+  console.log(process.env);
+
   const { url, resize_w, resize_h } = event.queryStringParameters;
-  const resizeParams = {
+  const params = {
     image: url,
     width: resize_w,
     height: resize_h,
   };
 
-  gmUtils.resize()(resizeParams).
-    then(outputStream => new Promise((resolve, reject) => {
-      const buffers = [];
-      const append = b => buffers.push(b);
-      outputStream.on('data', append);
-      outputStream.on('end', () => resolve(Buffer.concat(buffers).toString('base64')));
-    })).
-    // then(body => callback(null, {
-    then(body => callback(null, {
-      isBase64Encoded: true,
-      statusCode: 200,
-      headers: { "Content-Type": "image/jpeg" },
-      body
-    })).
-    catch(callback);
+  const noop = () => {};
+  const log = string => (fn = noop) => (...args) => { console.log(string); fn(...args) };
+
+  try {
+    var outputStream =
+      gm(request(params.image))
+        .resize(params.width, params.height)
+        .background('#FFF')
+        .flatten()
+        .stream('jpg');
+
+    const buffers = [];
+    const append = b => buffers.push(b);
+
+    outputStream.on('data', log('appending data...')(append));
+    outputStream.on('end', log('end')(() => callback(
+      null, 
+      {
+        isBase64Encoded: true,
+        statusCode: 200,
+        headers: { "Content-Type": "image/jpeg" },
+        body: Buffer.concat(buffers).toString('base64')
+      }
+    )));
+  } catch (err) {
+    console.error(err),
+    callback(err);
+  }
 };
