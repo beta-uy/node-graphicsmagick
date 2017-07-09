9919454f35144697d35b09d908d760cf19323e7c
Carlos Vega Meyer <carlosvega09@gmail.com>
2017-07-09 16:20:38 -0300

    Removes JSON wrapper around lambda result and plugs base64-encoded resize output straignt into callback for correct Lambda Function integration response decoding (no proxy option)

--- lambda.js
+++ lambda.js
@@ -2,8 +2,6 @@ var request = require('request');
 var gm = require('gm').subClass({ imageMagick: true });
 
 exports.handler = (event, context, callback) => {
-  console.log(process.env);
-
   const { url, resize_w, resize_h } = event.queryStringParameters;
   const params = {
     image: url,
@@ -25,16 +23,17 @@ exports.handler = (event, context, callback) => {
     const buffers = [];
     const append = b => buffers.push(b);
 
-    outputStream.on('data', log('appending data...')(append));
-    outputStream.on('end', log('end')(() => callback(
+    outputStream.on('data', append);
+    outputStream.on('end', () => callback(
       null, 
-      {
-        isBase64Encoded: true,
-        statusCode: 200,
-        headers: { "Content-Type": "image/jpeg" },
-        body: Buffer.concat(buffers).toString('base64')
-      }
-    )));
+      Buffer.concat(buffers).toString('base64')
+      // {
+      //   isBase64Encoded: true,
+      //   statusCode: 200,
+      //   headers: { "Content-Type": "image/jpeg" },
+      //   body: Buffer.concat(buffers).toString('base64')
+      // }
+    ));
   } catch (err) {
     console.error(err),
     callback(err);
