80edce15047884ca1276447117c6254693329cd1
Max Techera <maxi.techerag@gmail.com>
2017-10-03 19:09:19 -0300

    Add cache control header

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var app = require('express')();
-var bodyParser = require('body-parser');
-var gmUtils = require('./gmUtils.js');
-var addRequestId = require('express-request-id')();
-var lengthStream = require('length-stream');
-var Promise = require('bluebird');
+var app = require("express")();
+var bodyParser = require("body-parser");
+var gmUtils = require("./gmUtils.js");
+var addRequestId = require("express-request-id")();
+var lengthStream = require("length-stream");
+var Promise = require("bluebird");
 
 app.use(bodyParser.json());
 app.use(addRequestId);
@@ -13,25 +13,26 @@ app.use(addRequestId);
     -d "$(cat example.composite.json | sed s/\"/\\\"/g)" \
     -H 'Content-Type: application/json'
 */
-app.post('/gm/composite', (req, res) =>
+app.post("/gm/composite", (req, res) =>
   gmUtils.composite()(req.body).then(
     outputStream => {
-      res.setHeader('Content-Type', 'image');
+      res.setHeader("Content-Type", "image");
       outputStream.pipe(res);
     },
     error => {
       res.status(500);
       res.send(error.message);
-    }
-  ));
+    },
+  ),
+);
 
 /*
   curl localhost:85/gm/pipe \
     -d "$(cat example.pipe.json | sed s/\"/\\\"/g)" \
     -H 'Content-Type: application/json'
 */
-app.post('/gm/pipe', (req, res) => {
-  var requestName = `[${req.id}] /gm/pipe "${req.body.operationName || 'Anonymous'}"`;
+app.post("/gm/pipe", (req, res) => {
+  var requestName = `[${req.id}] /gm/pipe "${req.body.operationName || "Anonymous"}"`;
   console.time(requestName);
 
   var pipe = req.body.pipe || [];
@@ -41,31 +42,32 @@ app.post('/gm/pipe', (req, res) => {
     pipe,
     ({ ref: prevRef, outputStream: prevStream }, action) => {
       context[prevRef || "$_"] = prevStream;
-      return gmUtils[action.utility](context)(action)
-        .then(outputStream => ({
-          ref: action.ref,
-          outputStream
-        }));
+      return gmUtils[action.utility](context)(action).then(outputStream => ({
+        ref: action.ref,
+        outputStream,
+      }));
     },
-    Promise.resolve(context)
-  ).then(
-    ({ outputStream }) => {
-      res.setHeader('Content-Type', 'image/png');
+    Promise.resolve(context),
+  )
+    .then(({ outputStream }) => {
+      res.setHeader("Content-Type", "image/png");
       outputStream.pipe(res);
-    }
-  ).catch(
-    error => {
+    })
+    .catch(error => {
       res.status(500);
       res.send(error.message);
-    }
-  ).then(() => console.timeEnd(requestName));
+    })
+    .then(() => console.timeEnd(requestName));
 });
 
-app.get('/resize', (req, res) => {
-  var requestName = `[${req.id}] /resize "${req.body.operationName || 'Anonymous'}"`;
+app.get("/resize", (req, res) => {
+  var requestName = `[${req.id}] /resize "${req.body.operationName || "Anonymous"}"`;
   console.time(requestName);
 
-  res.setHeader('Content-Type', 'image/jpeg');
+  res.setHeader("Content-Type", "image/jpeg");
+  if (req.query.refresh) {
+    res.setHeader("Cache-Control", "max-age=" + req.query.refresh);
+  }
 
   var resizeParams = {
     image: req.query.url,
@@ -77,27 +79,29 @@ app.get('/resize', (req, res) => {
     if (length > 0) return;
 
     console.error(`[${req.id}] Got empty stream for query: ${JSON.stringify(req.query)}`);
+    res.setHeader("Cache-Control", "no-cache");
     res.status(503).end();
   });
 
-  gmUtils.resize()(resizeParams).then(
-    outputStream => outputStream
-      .pipe(earlyExitIfEmptyStream)
-      .pipe(res)
-      .on('finish', () => console.timeEnd(requestName))
-  ).catch(
-    error => {
+  gmUtils.resize()(resizeParams)
+    .then(outputStream =>
+      outputStream
+        .pipe(earlyExitIfEmptyStream)
+        .pipe(res)
+        .on("finish", () => console.timeEnd(requestName)),
+    )
+    .catch(error => {
+      res.setHeader("Cache-Control", "no-cache");
       res.status(500);
       res.send(error.message);
       console.timeEnd(requestName);
-    }
-  );
+    });
 });
 
 app.listen(80);
 console.log("Listening on port 80");
 
-process.on('SIGTERM', function() {
+process.on("SIGTERM", function() {
   console.log("Bye!");
   process.exit();
 });
